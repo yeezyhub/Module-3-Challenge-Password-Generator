53a763e96dab2d4c62a53901aca449de0a7d0f19
Yigit Ocak <yigitocak@Yigits-MBP.lan>
2022-10-01 19:49:28 -0500

    README.md is updated

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Module-3-Challenge-Password-Generator
+
+## Deployed Website
+
+
+
+## Description
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;People use almost same passwords everywhere and that makes us a target for hacking activities. Therefore we need stronger and unique passwords for every other login credentials to prevent hacking activities. Because of that reason, I have created a password generator that can solve creating random passwords, it is couple clicks away! Also, during the project I've learned how to use JavaScript functions with the help of some DOM elements.
+
+## Usage
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;To generate a password, click on "Generate Password" button. Then the page will ask you how long you want your password to be. After choosing that the prompts for choosing special, numeric, lower case and upper case will show up for you to choose. After the last question, the generated customized and secure password will be shown on the website inside the textbox so you can copy and paste it to store it.
+
+<!-- <img src="./assets/images/yeezy-my-portfolio.png" width="50%" height="50%"> -->
+
+## Credits
+
+My Github Profile: [yeezyhub](https://github.com/yeezyhub)
+
+## License
+
+Please refer to the LICENSE in the repo.
--- script.js
+++ script.js
@@ -1,25 +1,22 @@
 // Assignment code here
 
-
-
-
 var allCharactersRandom = [];
 var generatedPassword = [];
 
 function generatePassword(){
   
-  var charLength = window.prompt("How many characters would you like your password to contain? Your password must be between 8-128 characters." );
+  var charLength = window.prompt("How many characters would you like your password to contain? Your password must be between 8-128 characters." ); //ask the user to put an input
   if(8 <= charLength && charLength <= 128 ){
-
-    var isSpecialChar = window.confirm("Click OK to confirm including special characters.");
+    
+    var isSpecialChar = window.confirm("Click OK to confirm including special characters."); //ask the user if they want to include special characters
       if(isSpecialChar){
-        // Special characters (space and backslash is not used)
+        // Special characters
         var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", " < ", "=", " > ", " ? ", "@", "[" , "]", " ^ ", "_", "`", "{", "|", "}", "~"];
       }else{
         var specialChar = [];
       }
 
-    var isNumericChar = window.confirm("Click OK to confirm including numeric characters.");
+    var isNumericChar = window.confirm("Click OK to confirm including numeric characters."); //ask the user if they want to include numeric characters
       if(isNumericChar){
         // Numeric characters
         var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -27,7 +24,7 @@ function generatePassword(){
         var numbers = [];
       }
 
-      var isLowerCaseChar = window.confirm("Click OK to confirm including lower case characters.");
+      var isLowerCaseChar = window.confirm("Click OK to confirm including lower case characters."); //ask the user if they want to include lower case characters
       if(isLowerCaseChar){
         // Alphabetical lower case characters
         var alphabethLowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -35,35 +32,53 @@ function generatePassword(){
         var alphabethLowerCase = [];
       }
 
-      var isUpperCaseChar = window.confirm("Click OK to confirm including upper case characters.");
+      var isUpperCaseChar = window.confirm("Click OK to confirm including upper case characters."); //ask the user if they want to include upper case characters
       if(isUpperCaseChar){
         // Alphabetical upper case characters
         var alphabethUpperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
       }else{
         var alphabethUpperCase = [];
       }
+
+      //combines all the arrays into one
+      var allCharacters = [...specialChar, ...numbers, ...alphabethLowerCase, ...alphabethUpperCase]; 
       
-      var allCharacters = [...specialChar, ...numbers, ...alphabethLowerCase, ...alphabethUpperCase];
-      
+      //prompts user to choose at least one type of characters
+      if (!isSpecialChar && !isNumericChar && !isLowerCaseChar && !isUpperCaseChar) {
+        window.alert('You have to choose at least one type of character type to generate a password.');
+      }else{
+
+      // randomly selecting array elements and filling another array with random characters
       for (let i = 0; i < charLength; i++) {
 
         allCharactersRandom[i] = Math.floor(Math.random()*(allCharacters.length+1)); // maximum limits: 0 <= x <= 188
         generatedPassword[i] = allCharacters[allCharactersRandom[i]];
 
       }
+        // if (generatedPassword.length != charLength) {
+        //   for (let i = 0; i < 10; i++) {
+            
+            
+        //     if (generatedPassword.length == charLength) {
+        //       return generatedPassword = generatedPassword.push(allCharactersRandom[i]);
+        //     }
+        //   }
+          
+        // } else {
+        //   //join method is used to erase unwanted characters and replace method is to remove spaces in between
+        // return generatedPassword.join("").replace(/\s/g,'');
+        // }
+        return generatedPassword.join("").replace(/\s/g,'');
+      
 
-      // window.alert("The password is: " + generatedPassword.join(""));
-
-      return generatedPassword.join("");
-
-
-  }else{
     window.alert("Wrong input, try again.");
     generatePassword();
+      }
+  }
   }
 
 
-}
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
